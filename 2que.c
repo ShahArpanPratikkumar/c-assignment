1865c503e72e46e619741cc21c598b896d270d6c
ShahArpanPratikkumar <shaharpan.cg@gmail.com>
2026-02-15 10:45:00 +0530

    Add C assignment question solutions 1que.c and 2que.c

---
+++ 2que.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        printf("%d\n", i);
+                      // if(i % 2 == 1){
+                      //     printf("5 natural number",i);
+                      // }
+    }
+
+    return 0;
+}
